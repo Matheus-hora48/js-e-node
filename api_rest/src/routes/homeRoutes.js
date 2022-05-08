0c557634d8c79a0384f029335123a58b254609f0
matheus-hora48 <matheus11hora@gmail.com>
2022-05-07 23:22:30 -0300

    Novos

---
+++ api_rest/src/routes/homeRoutes.js
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+import homeControllers from '../controllers/HomeControllers';
+
+const router = new Router();
+router.get('/', homeControllers.index);
+
+export default router;
