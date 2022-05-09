f430d1b51b9f76d2b4d95320d948eb9d0c36a07d
Matheus Hora <matheus11hora@gmail.com>
2022-05-09 18:00:02 -0300

    Novos

--- api_rest/src/config/database.js
+++ api_rest/src/config/database.js
@@ -1,12 +1,20 @@
 require('dotenv').config();
 
 module.exports = {
-  dialect: 'mariadb',
+
+  /* MySQL / MariaDB */
+  dialect: 'mysql',
   host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE,
+  dialectOptions: {
+    timezone: 'America/Sao_Paulo',
+  },
+  timezone: 'America/Sao_Paulo',
+
+  /* ALL */
   define: {
     timestamps: true,
     underscored: true,
@@ -14,8 +22,4 @@ module.exports = {
     createdAt: 'created_at',
     updatedAt: 'updated_at',
   },
-  dialectOptions: {
-    timezone: 'America/Sao_Paulo',
-  },
-  timezone: 'America/Sao_Paulo',
 };
