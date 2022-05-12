439101d1ebace9ed56697f86a1cd00d5d01b5084
Matheus Hora <matheus11hora@gmail.com>
2022-05-12 10:52:20 -0300

    Novos

--- api_rest/.eslintrc.js
+++ api_rest/.eslintrc.js
@@ -3,16 +3,18 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ["airbnb-base"],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {
-    'no-console': 'off',
-    'class-methods-use-this': 'off',
-    'import/first': 'off',
+    "no-console": "off",
+    "class-methods-use-this": "off",
+    "import/first": "off",
+    "no-param-reassign": "off",
+    "linebreak-style": "off",
+    quotes: "off",
+    "quotes-props": "off",
   },
 };
