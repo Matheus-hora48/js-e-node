2b89aa1a2f09c896c23ee934a207e70ed03cc6db
Matheus-hora48 <matheus11hora@gmail.com>
2022-05-20 13:37:39 -0300

    Novos

--- new_api/app.js
+++ new_api/app.js
@@ -1,3 +1,19 @@
-export default () => {
-  console.log("Olá mundo!");
-};
+import express from "express";
+import homeRoutes from './src/routes/homeRoutes'
+
+class App {
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.App(express.json());
+  }
+
+  routes() {}
+}
+
+export default new App().app;
