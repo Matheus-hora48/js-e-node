fa45b558cd4c8005378b9dad78cec8b0f103e81c
Matheus-hora48 <matheus11hora@gmail.com>
2022-05-23 14:27:16 -0300

    Novos

--- new_api/src/controllers/homeController.js
+++ new_api/src/controllers/homeController.js
@@ -2,19 +2,15 @@ import Aluno from "../models/aluno";
 
 class HomeController {
   async index(req, res) {
-    try {
-      const novoAluno = await Aluno.create({
-        nome: "Matheus",
-        sobrenome: "Hora",
-        email: "teste@email.com",
-        idade: 20,
-        peso: 78,
-        altura: 1.7,
-      });
-      res.json(novoAluno);
-    } catch (e) {
-      console.log(e);
-    }
+    const novoAluno = await Aluno.create({
+      nome: "Matheus",
+      sobrenome: "Hora",
+      email: "teste@email.com",
+      idade: 20,
+      peso: 78,
+      altura: 1.7,
+    });
+    res.json(novoAluno);
   }
 }
 
