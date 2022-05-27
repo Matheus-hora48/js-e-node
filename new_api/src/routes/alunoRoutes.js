c723fb3ac5860d171f74a2231d3d34a0993642c9
Matheus-hora48 <matheus11hora@gmail.com>
2022-05-27 14:30:50 -0300

    Novos

--- new_api/src/routes/alunoRoutes.js
+++ new_api/src/routes/alunoRoutes.js
@@ -4,4 +4,8 @@ import alunoController from "../controllers/alunoController";
 const router = new Router();
 
 router.get("/", alunoController.index);
+router.post("/", alunoController.store);
+router.put("/", alunoController.update);
+router.get("/", alunoController.show);
+router.delete("/", alunoController.delete);
 export default router;
